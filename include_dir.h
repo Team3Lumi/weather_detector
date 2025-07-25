215d1ca786293970968ccbebb9d1b88efba168c8
Le Quang Huy <125816203+ep1co@users.noreply.github.com>
2025-07-26 00:11:24 +0700

    Create include_dir.h

---
+++ include_dir.h
@@ -0,0 +1,18 @@
+#ifndef INCLUDE_H
+#define INCLUDE_H
+
+
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "esp_wifi.h"
+#include "Wifi_Config.h"
+
+
+
+#endif
