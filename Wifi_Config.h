52ebe69647f1cd2289fb464bf8df49f230b03c59
NguyenNgocDai <133990881+0961021585@users.noreply.github.com>
2025-07-25 21:09:39 +0700

    Add files via upload

---
+++ Wifi_Config.h
@@ -0,0 +1,10 @@
+#ifndef WIFI_CONFIG_H
+#define WIFI_CONFIG_H
+
+// Tên mạng Wi-Fi (SSID)
+#define WIFI_SSID      "Tên_Mạng_Của_Bạn"
+
+// Mật khẩu Wi-Fi
+#define WIFI_PASS      "Mật_Khẩu_Của_Bạn"
+
+#endif // WIFI_CONFIG_H
