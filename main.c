8fcf117d8574afe9f02dcfa01b944277bdb66424
NguyenNgocDai <133990881+0961021585@users.noreply.github.com>
2025-08-01 10:22:00 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,4 @@
-#include "http.h"
-#include "Wifi.h"
-#include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_log.h"
+#include "include_dir.h"
 
 static const char *TAG = "wifi_sta";
 
