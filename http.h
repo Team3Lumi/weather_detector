2bb6c50721f0dc672e9fe53eef94983f0768ccb5
NguyenNgocDai <133990881+0961021585@users.noreply.github.com>
2025-08-09 21:35:23 +0700

    Update http.h

--- http.h
+++ http.h
@@ -2,11 +2,18 @@
 #define HTTP_H
 
 #include "esp_err.h"
+#include "esp_http_client.h"
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "esp_http_client.h"
+#include "cJSON.h"
+#include "esp_crt_bundle.h"
+#include "esp_heap_caps.h"
 
-#define GET_URL "http://httpbin.org/get"
+#define WEATHER_API_URL "https://api.open-meteo.com/v1/forecast?latitude=21.0&longitude=105.75&daily=temperature_2m_max,precipitation_probability_mean,weathercode&timezone=auto"
 
-void http_start_task(void);
-
-void http_start_task(void);
+void get_weather_forecast(void);
 
 #endif
