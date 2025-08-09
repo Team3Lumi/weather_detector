b4c908f99c418ec02ea8288f91f948e18676dd4d
NguyenNgocDai <133990881+0961021585@users.noreply.github.com>
2025-08-09 21:33:50 +0700

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,11 @@
 
 static const char *TAG = "wifi_sta";
 
+static void weather_task(void *arg) {
+    get_weather_forecast();
+    vTaskDelete(NULL);
+}
+
 void
 app_main(void){
     ESP_LOGI(TAG, "Starting weather station");
@@ -10,5 +15,6 @@ app_main(void){
     wifi_init_sta();
 
     // 3. Khởi động task HTTP
-    http_start_task();
+    //http_start_task();
+    xTaskCreate(weather_task, "weather", 10 * 1024, NULL, 5, NULL);
 }
